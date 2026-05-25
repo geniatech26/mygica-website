4ba5ba2f680747d54e66b104845256abdda519e6
geniatech26 <geniatech24@gmail.com>
2026-05-25 15:40:30 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'compile' // 启用构建时图片优化
+  }),
+  outDir: './dist', // 明确指定输出目录（Cloudflare Pages 默认寻找此目录）
   build: {
     format: 'file',
   },
